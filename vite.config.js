7a62cd3fe93c0e5d8c23cd0c327e8bbd583e1235
Siji <sijijoseph95@gmail.com>
2025-04-06 09:19:39 +1200

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || '/siji-joseph-nplt',
+  base: process.env.VITE_BASE_PATH || '/siji-joseph',
   css: {
     modules: {
       localsConvention: 'camelCase',
